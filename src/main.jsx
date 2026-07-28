ab1856b8136467dbde8d1137634243b1f4ba4137
Muhammad Fayiz K <fayizmuhammad440@gmail.com>
2026-07-28 19:13:36 +0530

    fix: resolve routing and asset paths for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Register from './pages/Register.jsx'
-import Home from './pages/Main/Home.jsx'
-import Menu from './pages/Menu.jsx'
-import Cart from './pages/Cart.jsx'
-import Profile from './pages/Profile.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
